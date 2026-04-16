92315a9e352f43f5d5c401148cfd0d4a627ab0b9
Giovanii7u7 <giovaniarango6@gmail.com>
2026-04-15 22:41:52 -0600

    feat: configurar react router con rutas base

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,37 @@
 import './App.css'
+import { NavLink, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home.jsx'
+import Alumnos from './pages/Alumnos.jsx'
+import Grupos from './pages/Grupos.jsx'
+import Calificaciones from './pages/Calificaciones.jsx'
+import Dashboard from './pages/Dashboard.jsx'
 
 function App() {
-  return <div />
+  return (
+    <div className="App">
+      <header className="App-header">
+        <nav>
+          <NavLink to="/" end>
+            Inicio
+          </NavLink>
+          <NavLink to="/alumnos">Alumnos</NavLink>
+          <NavLink to="/grupos">Grupos</NavLink>
+          <NavLink to="/calificaciones">Calificaciones</NavLink>
+          <NavLink to="/dashboard">Dashboard</NavLink>
+        </nav>
+      </header>
+
+      <main className="App-main">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/alumnos" element={<Alumnos />} />
+          <Route path="/grupos" element={<Grupos />} />
+          <Route path="/calificaciones" element={<Calificaciones />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </main>
+    </div>
+  )
 }
 
 export default App
---
+++ src/pages/Alumnos.jsx
@@ -0,0 +1,10 @@
+function Alumnos() {
+  return (
+    <section>
+      <h1>Alumnos</h1>
+      <p>Gestión de alumnos y datos personales.</p>
+    </section>
+  )
+}
+
+export default Alumnos
